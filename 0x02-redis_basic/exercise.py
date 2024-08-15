a48f949da4c143160cdfbba730bef93de042de21
0xAckerMan <joelkores1@gmail.com>
2024-08-15 05:46:34 +0300

    Reading from Redis and recovering original type

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+contains the defination of redis
+"""
+import redis
+from typing import Union, Callable, Optional
+import uuid
+
+
+class Cache:
+    """
+    Define a method for handling redis cache ops
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize redis client
+        Attr:
+            self._redis(redis.Redis): redis client
+        """
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in redis
+        """
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
